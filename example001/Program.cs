77b38cf5d5ce669d4feaefba8f604527469661fa
chiril200 <ionhawk397@gmail.com>
2023-09-18 18:22:11 +0300

    example001

---
+++ example001/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 3;
+int b = 8;
+int g = 9;
+int h = 2;
+int j = 1;
+int k = 0;
+int l = 7;
+int n = 3;
+int m = 5;
+
+int max = a;
+
+if ( a > max) max = a;
+if ( b > max) max = b;
+if ( g > max) max = g;
+if ( h > max) max = h;
+if ( j > max) max = j;
+if ( k > max) max = k;
+if ( l > max) max = l;
+if ( n > max) max = n;
+if ( m > max) max = m;
+
+Console.Write("max =");
+Console.WriteLine(max);
